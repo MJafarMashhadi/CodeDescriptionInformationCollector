08671bdc90bca1e954e92959a60092b6724e7fe1
MJafar Mashhadi <raindigital2007@gmail.com>
2016-01-07 13:01:28 +0330

    Really nice and detailed admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+models = []
+registration = map(admin.site.register, models)
+list(registration)  # apply, python 3 hack
+
+
+@admin.register(ProgrammingLanguage)
+class PLAdmin(admin.ModelAdmin):
+    list_display = ('name', 'object_oriented', 'functional', 'compiled', 'interpreted')
+
+
+@admin.register(UserKnowsPL)
+class UserKnowsPLAdmin(admin.ModelAdmin):
+    list_display = ('user', 'language', 'proficiency')
+
+
+class KnowLangaugeInline(admin.TabularInline):
+    model = UserKnowsPL
+    extra = 1
+
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'academic_degree', 'experience', 'is_active')
+    fields = (
+        ('first_name', 'last_name'),
+        ('email', 'password'),
+        ('academic_degree', 'experience'),
+        ('is_active', 'is_staff', 'is_superuser'),
+        ('date_joined', 'last_login')
+    )
+    inlines = [KnowLangaugeInline]
