75bc76c99a3490c292bed24555e52b234e517345
MJafar Mashhadi <raindigital2007@gmail.com>
2016-03-19 13:09:02 +0330

    Forgot to add media urls

--- InformationCollector/urls.py
+++ InformationCollector/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, patterns, url
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Examples:
@@ -9,7 +10,7 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('core.urls', namespace='core')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
