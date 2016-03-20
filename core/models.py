bbec247d981c96f6a105d52c602634efaa8fe7d0
MJafar Mashhadi <raindigital2007@gmail.com>
2016-03-20 09:25:49 +0330

    Allow . in username

--- core/models.py
+++ core/models.py
@@ -42,12 +42,12 @@ class Member(AbstractBaseUser, PermissionsMixin):
     """
     username = models.CharField(_('username'), max_length=30, unique=True,
         help_text=_('Required. 30 characters or fewer. Letters, digits and '
-                    '-/_ only.'),
+                    '-/./_ only.'),
         validators=[
-            validators.RegexValidator(r'^[\w-]+$',
+            validators.RegexValidator(r'^[\w.-]+$',
                                       _('Enter a valid username. '
                                         'This value may contain only letters, numbers '
-                                        'and -/_ characters.'), 'invalid'),
+                                        'and -/./_ characters.'), 'invalid'),
         ],
         error_messages={
             'unique': _("A user with that username already exists."),
