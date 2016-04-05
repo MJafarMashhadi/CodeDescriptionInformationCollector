8c9002499b917205c4b8ded0b60684cf094f7600
MJafar Mashhadi <raindigital2007@gmail.com>
2016-04-05 18:34:26 +0430

    serve media

--- InformationCollector/urls.py
+++ InformationCollector/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, patterns, url
 from django.contrib import admin
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     # Examples:
@@ -10,7 +10,8 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('core.urls', namespace='core')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }),
+]
 
 if settings.DEBUG:
     import debug_toolbar
