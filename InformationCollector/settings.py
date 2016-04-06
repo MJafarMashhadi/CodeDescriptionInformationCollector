f3745c097e8f5c9a863fa1feb8c947457f5db996
MJafar Mashhadi <raindigital2007@gmail.com>
2016-04-06 09:34:17 +0430

    Sentry

--- InformationCollector/settings.py
+++ InformationCollector/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = (
     'django.contrib.humanize',
     # apps
     'core',
-)
+) 
+if not DEBUG:
+    INSTALLED_APPS = INSTALLED_APPS + ('raven.contrib.django.raven_compat', )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -126,3 +128,12 @@ LOGIN_URL = 'core:login'
 DEBUG_TOOLBAR_CONFIG = {
     'JQUERY_URL': '',
 }
+
+# Raven
+if not DEBUG:
+    import raven
+
+    RAVEN_CONFIG = {
+        'dsn': 'https://b32f63a912054d01b45a48a515a50fcd:8b46738c65194064a0dc01547b5c8a18@sentry.nivad.io/3',
+        'release': raven.fetch_git_sha(os.path.dirname(os.path.dirname(__file__))),
+    }
