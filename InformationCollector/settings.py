0e52e9ddcbd0be740e87b4bd199553421f231b8d
MJafar Mashhadi <raindigital2007@gmail.com>
2016-04-07 14:38:43 +0430

    Add loggers

--- InformationCollector/settings.py
+++ InformationCollector/settings.py
@@ -126,3 +126,35 @@ LOGIN_URL = 'core:login'
 DEBUG_TOOLBAR_CONFIG = {
     'JQUERY_URL': '',
 }
+
+
+if not DEBUG:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': True,
+        'root': {
+            'level': 'INFO',
+            'handlers': ['console'],
+        },
+        'formatters': {
+            'verbose': {
+                'format': '%(levelname)s %(asctime)s %(module)s '
+                          '%(process)d %(thread)d %(message)s'
+            },
+        },
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'verbose'
+            }
+        },
+        'loggers': {
+            'django.db.backends': {
+                'level': 'ERROR',
+                'handlers': ['console'],
+                'propagate': False,
+            },
+        },
+    }
+
