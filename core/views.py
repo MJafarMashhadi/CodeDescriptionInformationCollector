a1594d408e6044051cdc95201dc97ecab18864a9
MJafar Mashhadi <raindigital2007@gmail.com>
2016-04-10 13:27:29 +0430

    Use of tabs and spaces

--- core/views.py
+++ core/views.py
@@ -238,11 +238,11 @@ def submit_new_snippet(request):
             
             count = CodeSnippet.objects.filter(submitter=request.user, approved=True).count()
             if count == 1:
-				request.user.earn_badge('code_submitter')
-			elif count == 3:
-				request.user.earn_badge('code_submitter_2')
-			elif count == 4:
-				request.user.earn_badge('code_submitter_3')
+                request.user.earn_badge('code_submitter')
+            elif count == 3:
+                request.user.earn_badge('code_submitter_2')
+            elif count == 4:
+                request.user.earn_badge('code_submitter_3')
 
             saved = True
     else:
